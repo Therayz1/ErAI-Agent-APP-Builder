0ee5b4fc411676ea1891124506355aff2d86f641
Eray Coşkun <96257290+Therayz1@users.noreply.github.com>
2025-04-12 19:26:41 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-  },
+  // swcMinify ve appDir kaldırıldı
 }
 
 module.exports = nextConfig
